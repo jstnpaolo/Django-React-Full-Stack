7a93731b15b983a8133bd670be090eb525f6fd42
JustineLopez <jus.paololopez@gmail.com>
2024-05-06 16:33:32 +0800

    First Commit

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,59 @@
+import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import api from "../api";
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
+import { useState, useEffect } from "react";
+
+function ProtectedRoute({ children }) {
+  const [isAuthorized, setIsAuthorized] = useState(null);
+
+  useEffect(() => {
+    auth().catch(() => setIsAuthorized(false));
+  }, []);
+
+  const refreshToken = async () => {
+    // is to retrieve the value of REFRESH_TOKEN from the browser's localStorage.
+    const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+    try {
+      const res = await api.post("/api/token/refresh/", {
+        refresh: refreshToken,
+      });
+      if (res.status === 200) {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        setIsAuthorized(true);
+      } else {
+        setIsAuthorized(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsAuthorized(false);
+    }
+  };
+
+  const auth = async () => {
+    // is to retrieve the value of ACCESS_TOKEN from the browser's localStorage.
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (!token) {
+      setIsAuthorized(false);
+      return;
+    }
+    const decoded = jwtDecode(token);
+    const tokenExpiration = decoded.exp;
+    const now = Date.now() / 1000;
+
+    if (tokenExpiration < now) {
+      // the tokenExpiration value is extracted from the decoded JWT payload (decoded.exp).
+      await refreshToken();
+    } else {
+      setIsAuthorized(true);
+    }
+  };
+
+  if (isAuthorized === null) {
+    return <div>Loading...</div>;
+  }
+
+  return isAuthorized ? children : <Navigate to="/login" />;
+}
+
+export default ProtectedRoute;
